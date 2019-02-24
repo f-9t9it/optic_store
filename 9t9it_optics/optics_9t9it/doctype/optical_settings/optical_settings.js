2ff2d75e9cbe4650625973b83b7b3b575b53258a
Sun Howwrongbum <sun@libermatic.com>
2019-02-24 18:00:51 +0530

    Optical Settings: create this

---
+++ 9t9it_optics/optics_9t9it/doctype/optical_settings/optical_settings.js
@@ -0,0 +1,16 @@
+// Copyright (c) 2019, 9T9IT and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Optical Settings', {
+  refresh: function(frm) {
+    if (frm.doc.fixtures_installed !== 'Yes') {
+      frm.add_custom_button('Install Fixtures', async function() {
+        await frappe.call({
+          method: '9t9it_optics.api.install.setup_fixtures',
+        });
+        await frm.set_value('fixtures_installed', 'Yes');
+        frm.save();
+      });
+    }
+  },
+});
