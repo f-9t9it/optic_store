fe7de144f4be00f618dd28501f2a19995edd5ca1
Sun Howwrongbum <sun@libermatic.com>
2019-03-09 02:34:52 +0530

    Optical Prescription: set sph_reading before_insert

--- optic_store/optic_store/doctype/optical_prescription/optical_prescription.py
+++ optic_store/optic_store/doctype/optical_prescription/optical_prescription.py
@@ -4,7 +4,7 @@
 
 from __future__ import unicode_literals
 import frappe
-from frappe.utils import date_diff
+from frappe.utils import date_diff, flt
 from frappe.model.document import Document
 
 
@@ -12,3 +12,7 @@ class OpticalPrescription(Document):
     def validate(self):
         if self.expiry_date and date_diff(self.expiry_date, self.test_date) < 0:
             frappe.throw("Expiry Date cannot be before Test Date")
+
+    def before_insert(self):
+        self.sph_reading_right = flt(self.sph_right) + flt(self.add_right)
+        self.sph_reading_left = flt(self.sph_left) + flt(self.add_left)
