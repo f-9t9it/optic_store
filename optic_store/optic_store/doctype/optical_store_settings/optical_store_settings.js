bd32647f8650b347f91866f56cae8f7141f8ba7f
Sun Howwrongbum <sun@libermatic.com>
2019-03-27 23:51:10 +0530

    Settings: move code to bundle

--- optic_store/optic_store/doctype/optical_store_settings/optical_store_settings.js
+++ optic_store/optic_store/doctype/optical_store_settings/optical_store_settings.js
@@ -1,22 +1,4 @@
 // Copyright (c) 2019, 9T9IT and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Optical Store Settings', {
-  refresh: function(frm) {
-    frm.set_query('item_group', 'dashboard_item_groups', {
-      filters: [['is_group', '=', '0']],
-    });
-
-    // hack to enable this button during development
-    const development = true;
-    if (development || frm.doc.defaults_installed !== 'Yes') {
-      frm.add_custom_button('Setup Defaults', async function() {
-        await frappe.call({
-          method: 'optic_store.api.install.setup_defaults',
-        });
-        await frm.set_value('defaults_installed', 'Yes');
-        frm.save();
-      });
-    }
-  },
-});
+frappe.ui.form.on('Optical Store Settings', optic_store.optical_store_settings);
