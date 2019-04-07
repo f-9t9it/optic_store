8cab5b4eb76d031242c13a580680d79f4fdf64a1
Sun Howwrongbum <sun@libermatic.com>
2019-04-07 23:55:31 +0530

    Group Discount: add blank row on new

--- optic_store/public/js/scripts/group_discount.js
+++ optic_store/public/js/scripts/group_discount.js
@@ -2,6 +2,12 @@ export default {
   refresh: function(frm) {
     if (frm.doc.__islocal) {
       frm.get_field('discount_name').set_focus();
+      frappe.model.add_child(
+        frm.doc,
+        'Group Discount Brand Category',
+        'discounts'
+      );
+      frm.refresh_field('discounts');
     }
   },
 };
