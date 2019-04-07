704b10b54b007ec771b9d90d663e749f5e728a2e
Sun Howwrongbum <sun@libermatic.com>
2019-04-07 23:53:39 +0530

    Group Discount: replace Brands with Brand Category

--- optic_store/optic_store/doctype/group_discount_brand_category/group_discount_brand_category.py
+++ optic_store/optic_store/doctype/group_discount_brand_category/group_discount_brand_category.py
@@ -6,5 +6,5 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
-class GroupDiscountBrand(Document):
+class GroupDiscountBrandCategory(Document):
 	pass
