2991a53b69859c713f8f9f3771a745c106d25a03
Sun Howwrongbum <sun@libermatic.com>
2019-04-21 20:17:09 +0530

    Stock Transfer: add Stock Entry handlers

--- optic_store/optic_store/doctype/stock_transfer/stock_transfer.py
+++ optic_store/optic_store/doctype/stock_transfer/stock_transfer.py
@@ -4,7 +4,134 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
+from frappe.utils import now, get_datetime, flt
 from frappe.model.document import Document
+from functools import partial
+from toolz import merge, compose
+
+from optic_store.utils import pick, sum_by
+
 
 class StockTransfer(Document):
-	pass
+    def before_save(self):
+        if not self.outgoing_datetime:
+            self.outgoing_datetime = now()
+        self.set_missing_fields()
+
+    def on_submit(self):
+        if self.workflow_state == "In Transit":
+            warehouses = self.get_warehouses(incoming=False)
+            accounts = self.get_accounts()
+            ref_doc = _make_stock_entry(
+                merge(
+                    pick(["company"], self.as_dict()),
+                    warehouses,
+                    {"items": _map_items(warehouses, accounts)(self.items)},
+                    _destruct_datetime(self.outgoing_datetime),
+                )
+            )
+            self.set_ref_doc("outgoing_stock_entry", ref_doc)
+
+    def before_update_after_submit(self):
+        if not self.incoming_datetime:
+            self.incoming_datetime = now()
+        self.validate_dates()
+
+    def on_update_after_submit(self):
+        if self.workflow_state == "Received":
+            warehouses = self.get_warehouses(incoming=False)
+            accounts = self.get_accounts()
+            ref_doc = _make_stock_entry(
+                merge(
+                    pick(["company"], self.as_dict()),
+                    warehouses,
+                    {"items": _map_items(warehouses, accounts)(self.items)},
+                    _destruct_datetime(self.incoming_datetime),
+                )
+            )
+            self.set_ref_doc("incoming_stock_entry", ref_doc)
+
+    def set_missing_fields(self):
+        for item in self.items:
+            item.amount = flt(item.qty) * flt(item.basic_rate)
+            item.valuation_rate = item.amount / flt(item.qty)
+        self.total_value = sum_by("amount")(self.items)
+        self.total_qty = sum_by("qty")(self.items)
+
+    def validate_dates(self):
+        if get_datetime(self.outgoing_datetime) > get_datetime(self.incoming_datetime):
+            frappe.throw(_("Outgoing Datetime cannot be after Incoming Datetime"))
+
+    def set_ref_doc(self, field, ref_doc):
+        self.db_set(field, ref_doc)
+
+    def get_warehouses(self, incoming=False):
+        transit_warehouse = frappe.db.get_single_value(
+            "Optical Store Settings", "transit_warehouse"
+        )
+        return (
+            {"from_warehouse": self.source_warehouse, "to_warehouse": transit_warehouse}
+            if not incoming
+            else {
+                "from_warehouse": transit_warehouse,
+                "to_warehouse": self.target_warehouse,
+            }
+        )
+
+    def get_accounts(self):
+        company = frappe.get_doc("Company", self.company)
+        return {
+            "expense_account": company.stock_adjustment_account,
+            "cost_center": company.cost_center,
+        }
+
+
+def _destruct_datetime(dt):
+    _dt = get_datetime(dt)
+    return {"posting_date": _dt.date(), "posting_time": _dt.time()}
+
+
+def _map_items(warehouses, accounts):
+    make_item = compose(
+        partial(
+            merge,
+            {
+                "s_warehouse": warehouses.get("from_warehouse"),
+                "t_warehouse": warehouses.get("to_warehouse"),
+                "expense_account": accounts.get("expense_account"),
+                "cost_center": accounts.get("cost_center"),
+            },
+        ),
+        partial(
+            pick,
+            [
+                "item_code",
+                "qty",
+                "uom",
+                "basic_rate",
+                "amount",
+                "valuation_rate",
+                "serial_no",
+                "batch_no",
+            ],
+        ),
+        lambda x: x.as_dict(),
+    )
+    return partial(map, make_item)
+
+
+def _make_stock_entry(args):
+    doc = frappe.get_doc(
+        merge(
+            {
+                "doctype": "Stock Entry",
+                "purpose": "Material Transfer",
+                "set_posting_time": 1,
+            },
+            args,
+        )
+    )
+    doc.insert()
+    doc.submit()
+    return doc.name
