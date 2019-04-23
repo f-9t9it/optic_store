da15b5959f63813bd5368e4d60487dca9cfb3360
Sun Howwrongbum <sun@libermatic.com>
2019-04-23 08:31:24 +0530

    0.3.1

--- optic_store/public/js/index.js
+++ optic_store/public/js/index.js
@@ -31,7 +31,7 @@ frappe.ui.form.OpticalPrescriptionQuickEntryForm = frappe.ui.form.QuickEntryForm
   optical_prescription_qe
 );
 
-const __version__ = '0.3.0';
+const __version__ = '0.3.1';
 
 frappe.provide('optic_store');
 optic_store = {
