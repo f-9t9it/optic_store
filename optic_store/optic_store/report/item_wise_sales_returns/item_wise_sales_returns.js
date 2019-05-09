b55fe499acf8645e0f304f6adb0407143f25625e
Sun Howwrongbum <sun@libermatic.com>
2019-05-10 00:49:11 +0530

    Copied Item-wise Sales Returns

---
+++ optic_store/optic_store/report/item_wise_sales_returns/item_wise_sales_returns.js
@@ -0,0 +1,20 @@
+// Copyright (c) 2016, 	9t9it and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports['Item-wise Sales Returns'] = {
+  filters: [
+    {
+      fieldname: 'from_date',
+      label: __('From Date'),
+      fieldtype: 'Date',
+      default: frappe.datetime.month_start(),
+    },
+    {
+      fieldname: 'to_date',
+      label: __('To Date'),
+      fieldtype: 'Date',
+      default: frappe.datetime.month_end(),
+    },
+  ],
+};
