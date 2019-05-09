0b1c850cb6a84e8389e0239e5ef9d846cbf1d840
Sun Howwrongbum <sun@libermatic.com>
2019-05-09 23:20:42 +0530

    POS: add shortcuts to print and new

--- optic_store/public/js/pages/pos.js
+++ optic_store/public/js/pages/pos.js
@@ -387,15 +387,25 @@ export default function extend_pos(PosClass) {
     }
     bind_keyboard_shortcuts() {
       $(document).on('keydown', e => {
-        if (this.numeric_keypad && e.keyCode === 120) {
-          e.preventDefault();
-          e.stopPropagation();
-          if (this.dialog && this.dialog.is_visible) {
-            this.dialog.hide();
-          } else {
-            $(this.numeric_keypad)
-              .find('.pos-pay')
-              .trigger('click');
+        if (frappe.get_route_str() === 'pos') {
+          if (this.numeric_keypad && e.keyCode === 120) {
+            e.preventDefault();
+            e.stopPropagation();
+            if (this.dialog && this.dialog.is_visible) {
+              this.dialog.hide();
+            } else {
+              $(this.numeric_keypad)
+                .find('.pos-pay')
+                .trigger('click');
+            }
+          } else if (this.frm.doc.docstatus == 1 && e.ctrlKey && e.keyCode === 80) {
+            e.preventDefault();
+            e.stopPropagation();
+            this.page.btn_secondary.trigger('click');
+          } else if (e.ctrlKey && e.keyCode === 66) {
+            e.preventDefault();
+            e.stopPropagation();
+            this.page.btn_primary.trigger('click');
           }
         }
       });
