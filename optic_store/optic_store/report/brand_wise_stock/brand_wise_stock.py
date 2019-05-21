0710bb953f39591cc054854dad1582f6e4cf8290
Sun Howwrongbum <sun@libermatic.com>
2019-05-21 17:46:31 +0530

    Brand-wise Stock: create this

---
+++ optic_store/optic_store/report/brand_wise_stock/brand_wise_stock.py
@@ -0,0 +1,63 @@
+# Copyright (c) 2013, 9T9IT and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+from functools import partial
+from toolz import compose, pluck, concatv
+
+from optic_store.utils import pick
+
+
+def execute(filters=None):
+    columns = _get_columns()
+    keys = compose(list, partial(pluck, "fieldname"))(columns)
+    clauses, values = _get_filters(filters)
+    data = _get_data(clauses, values, keys)
+    return columns, data
+
+
+def _get_columns():
+    def make_column(key, label, type="Currency", options=None, width=120):
+        return {
+            "label": _(label),
+            "fieldname": key,
+            "fieldtype": type,
+            "options": options,
+            "width": width,
+        }
+
+    return [
+        make_column("brand", "Brand", type="Link", options="Brand", width=180),
+        make_column("qty", "Qty", type="Float"),
+    ]
+
+
+def _get_filters(filters):
+    clauses = concatv(
+        ["disabled = 0"],
+        ["i.brand = %(brand)s"] if filters.brand else [],
+        ["i.item_group = %(item_group)s"] if filters.item_group else [],
+        ["b.warehouse = %(warehouse)s"] if filters.warehouse else [],
+    )
+    return " AND ".join(clauses), filters
+
+
+def _get_data(clauses, values, keys):
+    items = frappe.db.sql(
+        """
+            SELECT
+                i.brand AS brand,
+                SUM(b.actual_qty) AS qty
+            FROM `tabItem` AS i
+            LEFT JOIN `tabBin` AS b ON b.item_code = i.item_code
+            WHERE {clauses}
+            GROUP BY i.brand
+        """.format(
+            clauses=clauses
+        ),
+        values=values,
+        as_dict=1,
+    )
+    return map(partial(pick, keys), items)
