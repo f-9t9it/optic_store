27d6aa5d7260b50a5f43710f7fcc39d135e2c314
Sun Howwrongbum <sun@libermatic.com>
2019-06-26 01:03:25 +0530

    feat(Simple Stock Balance): create this report

--- optic_store/public/js/reports/index.js
+++ optic_store/public/js/reports/index.js
@@ -1,3 +1,4 @@
 export { default as payment_summary } from './payment_summary';
 export { default as item_wise_stock } from './item_wise_stock';
 export { default as branch_stock } from './branch_stock';
+export { default as simple_stock_balance } from './simple_stock_balance';
