54e0d92d300f64e43302c3f5bb183ff3f96043be
Sun Howwrongbum <sun@libermatic.com>
2019-06-27 14:10:00 +0530

    feat(pos): empty sales person field on new

--- optic_store/public/js/addons/withSalesperson.js
+++ optic_store/public/js/addons/withSalesperson.js
@@ -12,6 +12,10 @@ export default function withSalesperson(Cart) {
       super.make_dom();
       this.wrapper.find('.cart-wrapper').after('<div class="sales_person-field" />');
     }
+    reset() {
+      super.reset();
+      this.sales_person_field.set_value(this.frm.doc.os_sales_person);
+    }
     async make_sale_sperson_field() {
       const sales_person_department = await frappe.db.get_single_value(
         'Optical Store Settings',
@@ -34,7 +38,7 @@ export default function withSalesperson(Cart) {
         parent: this.wrapper.find('.sales_person-field'),
         render_input: true,
       });
-      this.sales_person_field.set_value(this.frm.doc.customer);
+      this.sales_person_field.set_value(this.frm.doc.os_sales_person);
     }
   };
 }
