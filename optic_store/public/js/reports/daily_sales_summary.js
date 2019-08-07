5f0adca0819942471fb0ae6dde399966b030a633
Sun Howwrongbum <sun@libermatic.com>
2019-08-08 01:01:15 +0530

    refactor: remove unused code

--- optic_store/public/js/reports/daily_sales_summary.js
+++ optic_store/public/js/reports/daily_sales_summary.js
@@ -7,12 +7,6 @@ export default {
       method: 'optic_store.api.customer.get_user_branch',
     });
     rep.set_filter_value('branch', branch);
-    const branch_filter = rep.filters.find(({ fieldname }) => fieldname === 'hqm_view');
-    if (branch_filter) {
-      const has_role = ['Sales Manager'].some(role => frappe.user_roles.includes(role));
-      branch_filter.df.read_only = !has_role;
-      branch_filter.refresh();
-    }
   },
   filters: [
     make_date({ fieldname: 'posting_date', label: 'Date', reqd: 1 }),
