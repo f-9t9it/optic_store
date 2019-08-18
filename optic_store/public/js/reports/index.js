718275b1b27ef22c4e09046999c2251be3976794
Sun Howwrongbum <sun@libermatic.com>
2019-08-18 22:09:24 +0530

    feat: add custom for Customer-wise Invoice in Customer form

--- optic_store/public/js/reports/index.js
+++ optic_store/public/js/reports/index.js
@@ -6,4 +6,6 @@ export { default as stock_transfer } from './stock_transfer';
 export { default as sales_summary_by_product } from './sales_summary_by_product';
 export { default as item_sell_out_history } from './item_sell_out_history';
 export { default as daily_sales_summary } from './daily_sales_summary';
-export { default as customer_wise_invoice } from './customer_wise_invoice';
+
+import * as utils from './fields';
+export { utils };
