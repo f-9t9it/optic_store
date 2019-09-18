49748253775d9fa57d1f1a8d6e28677b27eb446b
Sun Howwrongbum <sun@libermatic.com>
2019-09-19 00:20:40 +0530

    fix(Employee): hide file attachments

--- optic_store/public/js/scripts/employee.js
+++ optic_store/public/js/scripts/employee.js
@@ -2,4 +2,13 @@ import { set_nationality_options } from './customer';
 
 export default {
   onload: set_nationality_options,
+  refresh: function(frm) {
+    if (
+      !['HR Manager', 'Account Manager', 'System Manager'].some(role =>
+        frappe.user_roles.includes(role)
+      )
+    ) {
+      frm.$wrapper.find('.form-sidebar > .form-attachments').hide();
+    }
+  },
 };
