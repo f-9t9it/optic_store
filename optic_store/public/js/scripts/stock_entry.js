771272d353a596789c3643e29c262f8c332842e2
Sun Howwrongbum <sun@libermatic.com>
2019-09-21 18:27:52 +0530

    fix(Stock Transfer): show all purposes for Stock Manager

--- optic_store/public/js/scripts/stock_entry.js
+++ optic_store/public/js/scripts/stock_entry.js
@@ -1,9 +1,14 @@
 export default {
   onload: function(frm) {
-    frm.set_df_property('purpose', 'options', ['Material Transfer']);
+    if (!['Stock Manager'].some(role => frappe.user_roles.includes(role))) {
+      frm.set_df_property('purpose', 'options', ['Material Transfer']);
+    }
   },
   refresh: function(frm) {
-    if (frm.doc.__islocal) {
+    if (
+      !['Stock Manager'].some(role => frappe.user_roles.includes(role)) &&
+      frm.doc.__islocal
+    ) {
       frm.set_value('purpose', 'Material Transfer');
     }
   },
