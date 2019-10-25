2897a27b062b5ac8a3fe9916315cc2e726a159e1
Sun Howwrongbum <sun@libermatic.com>
2019-10-25 22:59:56 +0530

    feat(Customer Loyalty Entry): handle cancel

--- optic_store/optic_store/doctype/custom_loyalty_entry/custom_loyalty_entry.py
+++ optic_store/optic_store/doctype/custom_loyalty_entry/custom_loyalty_entry.py
@@ -4,6 +4,7 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
 from frappe.utils import add_days, flt
 from frappe.model.document import Document
 from erpnext.accounts.doctype.loyalty_program.loyalty_program import (
@@ -33,7 +34,11 @@ class CustomLoyaltyEntry(Document):
         else:
             self._deduct_points()
 
+    def on_cancel(self):
+        self._delete_entry()
+
     def _add_points(self):
+        # copied from sales_invoice:make_loyalty_point_entry
         frappe.get_doc(
             {
                 "doctype": "Loyalty Point Entry",
@@ -49,6 +54,7 @@ class CustomLoyaltyEntry(Document):
         ).save(ignore_permissions=1)
 
     def _deduct_points(self):
+        # copied from sales_invoice:apply_loyalty_points
         loyalty_point_entries = get_loyalty_point_entries(
             self.customer, self.loyalty_program, self.company, self.posting_date
         )
@@ -79,5 +85,17 @@ class CustomLoyaltyEntry(Document):
                 }
             ).save(ignore_permissions=1)
             points_to_redeem -= redeemed_points
-            if points_to_redeem < 1:  # since points_to_redeem is integer
+            if points_to_redeem < 1:
                 break
+
+    def _delete_entry(self):
+        lpe = frappe.db.exists(
+            "Loyalty Point Entry", {"os_custom_loyalty_entry": self.name}
+        )
+        if not lpe:
+            return
+
+        if frappe.get_all("Loyalty Point Entry", filters={"redeem_against": lpe}):
+            frappe.throw(_("Cannot be cancelled because points are already redeemed"))
+
+        frappe.delete_doc("Loyalty Point Entry", lpe)
