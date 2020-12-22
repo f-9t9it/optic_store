667a3fb910abb5c3b91e704077fc5771dbd36f60
Ivan Ray Altomera <irayspacii@gmail.com>
2020-12-22 18:07:42 +0800

    feat(desktop): added POS and XZ Report

--- optic_store/config/desktop.py
+++ optic_store/config/desktop.py
@@ -7,9 +7,33 @@ def get_data():
     return [
         {
             "module_name": "Optic Store",
+            "category": "Modules",
+            "label": _("Optic Store"),
             "color": "grey",
             "icon": "octicon octicon-file-directory",
             "type": "module",
-            "label": _("Optic Store"),
+            "link": "#point-of-sale",
+        },
+        {
+            "module_name": "Point of Sale",
+            "category": "Modules",
+            "label": _("Point of Sale"),
+            "color": "#FF4136",
+            "icon": "fa fa-shopping-cart",
+            "type": "link",
+            "standard": 1,
+            "link": "#point-of-sale",
+            "onboard_present": 1
+        },
+        {
+            "module_name": "XZ Report",
+            "category": "Modules",
+            "label": _("XZ Report"),
+            "color": "#FF4136",
+            "icon": "fa fa-tasks",
+            "type": "link",
+            "standard": 1,
+            "link": "#List/XZ Report",
+            "onboard_present": 1
         }
     ]
