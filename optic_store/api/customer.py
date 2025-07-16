f7121afc1f8b319c5b4e60ffa2d9b263be0d7f30
Doreen2002 <doreenmwapekatebe8@gmail.com>
2025-07-16 17:44:08 +0200

    fic filter from branch users

--- optic_store/api/customer.py
+++ optic_store/api/customer.py
@@ -25,13 +25,14 @@ CUSTOMER_DETAILS_FIELDS = [
 
 @frappe.whitelist()
 def get_user_branch(user=None):
-    branch = frappe.db.exists("Branch", {"os_user": user or frappe.session.user})
-    if branch:
-        return branch
-    employee = frappe.db.exists("Employee", {"user_id": user or frappe.session.user})
-    if employee:
-        return frappe.db.get_value("Employee", employee, "branch")
-    return None
+    branch = frappe.db.get_all("Branch Users", filters={"branch_user": user or frappe.session.user}, fields=['parent'] )
+    branch_exists = frappe.db.exists("Branch Users", {"branch_user": user or frappe.session.user})
+    if branch_exists:
+        return branch[0].parent if branch != [] else None
+    # employee = frappe.db.exists("Employee", {"user_id": user or frappe.session.user})
+    # if employee:
+    #     return frappe.db.get_value("Employee", employee, "branch")
+    # return None
 
 
 @frappe.whitelist()
