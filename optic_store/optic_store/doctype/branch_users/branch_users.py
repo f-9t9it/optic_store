e3a81adfcb37f757f607d443eebb56b3fbcb6329
Doreen2002 <doreenmwapekatebe8@gmail.com>
2025-07-16 20:26:51 +0200

    add branch usrs child table

---
+++ optic_store/optic_store/doctype/branch_users/branch_users.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025, 9T9IT and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class BranchUsers(Document):
+	pass
